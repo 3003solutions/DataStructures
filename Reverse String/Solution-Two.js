46799469d087c1c92566c1b9fd37adf9a52d57a0
3003solutions <3003solutions@gmail.com>
2021-06-29 17:10:40 -0700

    Update Solution-Two.js

--- Reverse String/Solution-Two.js
+++ Reverse String/Solution-Two.js
@@ -1,1 +1,10 @@
+function reverse(str){
 
+  const arr = str.split('');   // Splitting our string into an array and assigning it to arr veriable.
+
+  arr.reverse();               // Reversing the array using reverse helper
+  
+  return arr.join('');        // Joining the array in to string again
+}
+
+reverse('Shawn');
