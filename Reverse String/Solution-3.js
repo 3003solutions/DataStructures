7dbd5154ddc4fc344376c12f1fa76f7cc20e2425
3003solutions <3003solutions@gmail.com>
2021-06-30 10:15:18 -0700

    Update Solution-3.js

--- Reverse String/Solution-3.js
+++ Reverse String/Solution-3.js
@@ -1,1 +1,15 @@
+function reverse(str) {
+  
+  let reversed = "";
+  
+  for(let character of str) {                     // creating a temporary veriable (character) of the object that we are iterating (str)
+    
+    reversed = character + reversed;              // We add that character on the string reversed
+  
+  }
+  
+  return reversed;                                // Now we return the string reversed 
+  
+}
 
+reverse("Shawn");                             
