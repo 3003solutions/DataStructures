11c23ce5a4aa5708f101f4cac1321323c1ec214d
3003solutions <3003solutions@gmail.com>
2021-06-30 10:46:18 -0700

    Update Solution-4.js

--- Reverse String/Solution-4.js
+++ Reverse String/Solution-4.js
@@ -1,6 +1,6 @@
 function reverse(str) {
 
- return str.split('').reduce((reversed, character) => {   // turning str in to an array and than condencing all values into a singlar one.
+ return str.split('').reduce((reversed, character) => {   // turning str in to an array and than condencing all values into a singlar one using array helper.
 
     return character + reversed                          // returning character plus reversed.
     
