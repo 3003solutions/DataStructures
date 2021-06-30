688637ecf0d07041e667a1ce89b0526f917a095f
3003solutions <3003solutions@gmail.com>
2021-06-30 10:45:21 -0700

    Update Solution-4.js

--- Reverse String/Solution-4.js
+++ Reverse String/Solution-4.js
@@ -2,7 +2,7 @@ function reverse(str) {
 
  return str.split('').reduce((reversed, character) => {   // turning str in to an array and than condencing all values into a singlar one.
 
-    return character + reversed;                          // returning character plus reversed.
+    return character + reversed                          // returning character plus reversed.
     
   }, '');   
   
