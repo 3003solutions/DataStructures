6d85d18d967d7252491ea47a86b19adc2b98a1de
3003solutions <3003solutions@gmail.com>
2021-07-15 11:01:33 -0700

    Update Solution-2.js

--- Anagrams/Solution-2.js
+++ Anagrams/Solution-2.js
@@ -1,1 +1,10 @@
+function anagrams(stringA, stringB){                                          //simpler solution to anagrams
+  return cleanString(stringA) === cleanString(stringB);
+}
 
+function cleanString(str){
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');              // clean method.
+}
+
+anagrams('rail safety', 'fairy tales')
+anagrams('Hi there', 'Bye there')
