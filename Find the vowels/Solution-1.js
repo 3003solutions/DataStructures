a457192a266b483fbc387ab46026447dacdaba8e
3003solutions <3003solutions@gmail.com>
2021-07-16 14:01:56 -0700

    Create Solution-1.js

---
+++ Find the vowels/Solution-1.js
@@ -0,0 +1,17 @@
+function vowels(str){
+
+  let count = 0;
+
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()){
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
+
+  return count; 
+  
+}
+
+vowels("Why do you ask?")
