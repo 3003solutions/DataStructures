77ec97ba9522b928b6ee86c1215ed0efb86533d3
3003solutions <3003solutions@gmail.com>
2021-07-28 23:56:28 -0700

    Create ChessBoard-Java.js

---
+++ ChessBoard/ChessBoard-Java.js
@@ -0,0 +1,21 @@
+public class MyClass {
+    public static void main(String args[]) {
+        
+        int size = 8;
+        
+        for (int i = 0;i < size;i++){
+            for(int j=0;j < size;j++){
+                if((i+j)%2 == 0)
+                
+                System.out.println('W');
+                
+                else
+                
+                System.out.println('B');
+            }
+            
+        }
+    
+    
+    }
+}
